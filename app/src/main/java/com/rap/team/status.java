a36ee064b3b5c1162a4ce1d179aece7081844c5d
Praveen Singh Rajput <Praveen Singh Rajput>
2016-01-16 19:39:39 +0530

    Initial commit

---
+++ app/src/main/java/com/rap/team/status.java
@@ -0,0 +1,15 @@
+package com.rap.team;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rap.team.teamregistration.R;
+
+public class status extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+}
