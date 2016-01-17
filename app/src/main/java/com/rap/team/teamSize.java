12327369fee012496b043e3a5e0e2a03125323c8
ajay mahicha <ajaymahicha@gmail.com>
2016-01-17 19:01:31 +0530

    team size page updated

--- app/src/main/java/com/rap/team/teamSize.java
+++ app/src/main/java/com/rap/team/teamSize.java
@@ -25,9 +25,14 @@ public class teamSize extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int a = rg.getCheckedRadioButtonId();
+                RadioButton a = (RadioButton) findViewById( rg.getCheckedRadioButtonId());
                       Bundle bundle =new Bundle();
-                bundle.putInt("teamsize",a);
+                int aa=2;
+                if(a.getText().equals("3 AGENTS"))
+                {
+                    aa=3;
+                }
+                bundle.putInt("teamsize",aa);
                 bundle.putInt("currentmember",1);
                 Intent intent = new Intent(teamSize.this, com.rap.team.enterDetail.class);
                 intent.putExtras(bundle);
