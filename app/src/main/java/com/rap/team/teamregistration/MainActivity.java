1a940d2958e7d99d9a9a00a12c43593da11c7c5b
ritikvijay <coolricks96@gmail.com>
2016-01-17 18:41:39 +0530

    AndroidManifest edited.

--- app/src/main/java/com/rap/team/teamregistration/MainActivity.java
+++ app/src/main/java/com/rap/team/teamregistration/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import com.rap.team.teamregistration.R;
+import com.rap.team.teamSize;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         acceptbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), com.rap.team.teamSize.class);
+                Intent intent = new Intent(v.getContext(), teamSize.class);
                 startActivityForResult(intent, 0);
             }
         });
