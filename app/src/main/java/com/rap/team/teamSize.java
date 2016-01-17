0b6d9cc88d09f0de977f7acb59eff657697e27fb
ajay mahicha <ajaymahicha@gmail.com>
2016-01-17 21:57:38 +0530

    Solved Conflicting Error-2

--- app/src/main/java/com/rap/team/teamSize.java
+++ app/src/main/java/com/rap/team/teamSize.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
 import com.rap.team.teamregistration.R;
 
@@ -25,8 +24,14 @@ public class teamSize extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int a = rg.getCheckedRadioButtonId();
-                      Bundle bundle =new Bundle();
+                int a=2;
+              RadioButton r=(RadioButton)findViewById(rg.getCheckedRadioButtonId());
+                if(r.getText().equals("3 AGENTS"))
+                {
+                    a = 3;
+                }
+
+                Bundle bundle =new Bundle();
                 bundle.putInt("teamsize",a);
                 bundle.putInt("currentmember",1);
                 Intent intent = new Intent(teamSize.this, com.rap.team.enterDetail.class);
