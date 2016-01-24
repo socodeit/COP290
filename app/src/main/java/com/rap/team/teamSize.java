a035394488c34bf7deeafd2aab43e06a005d6f53
ajay mahicha <ajaymahicha@gmail.com>
2016-01-24 21:48:28 +0530

    commenting done & toast message updated in team size page

--- app/src/main/java/com/rap/team/teamSize.java
+++ app/src/main/java/com/rap/team/teamSize.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.rap.team.teamregistration.R;
 
@@ -19,30 +20,42 @@ public class teamSize extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_size);
-        rg = (RadioGroup) findViewById(R.id.radioGroup);
-        rg.clearCheck();
-
+        rg = (RadioGroup) findViewById(R.id.radioGroup); //using radiobutton to choose team s ize
         Button next = (Button) findViewById(R.id.buttonsize);
-        next.setOnClickListener(new View.OnClickListener() {
+        next.setOnClickListener(new View.OnClickListener() {         //defining what to do when next button is pressed
             @Override
             public void onClick(View v) {
                 int a=2;
               RadioButton r=(RadioButton)findViewById(rg.getCheckedRadioButtonId());
+                //changing value of in a according to team size chosen
                 if(r.getText().equals("3 AGENTS"))
                 {
                     a = 3;
                 }
-
+                // creating bundle to pass team name and team size details
                 Bundle bundle =new Bundle();
                 EditText nameField = (EditText) findViewById(R.id.teamnam);
                 Editable editable = nameField.getText();
                 String name = editable == null ? "" : editable.toString();
+                String err="";
+                //checking if team name is not entered and next button is pressed then showing toast message to fill team name box
+                if(name.equals("")) {
+                    err += "Team Name can't be empty..!\n";
+                    Toast.makeText(getApplicationContext(), err, Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    //if everything is fine then putting team name and size in bundle
                 bundle.putString("teamname",name);
                 bundle.putInt("teamsize",a);
                 bundle.putInt("currentmember",1);
+                    //creating intent to open enterdetail as next page
                 Intent intent = new Intent(teamSize.this, com.rap.team.enterDetail.class);
+                    //putting bundle in intent
                 intent.putExtras(bundle);
+                    //opening next(enterdetail) page
                 startActivity(intent);
+                }
             }
         });
     }
